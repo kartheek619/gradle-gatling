90fe175afd84c2123c2837ead1197d2bbb95c194
Kartheek.Maremalla <z002b1c@9801a793b113.hq.target.com>
2016-09-06 16:07:25 -0500

    Adding comments

--- src/test/scala/gatling/simulations/SampleSimulation.scala
+++ src/test/scala/gatling/simulations/SampleSimulation.scala
@@ -5,10 +5,10 @@ import io.gatling.http.Predef._
 
 class SampleSimulation extends Simulation {
   val httpProtocol = http
-    .baseURL("http://www.google.com")
+    .baseURL("http://www.google.com")    //Replace google url with url you want to test
 
-  val scn = scenario("foo load test")
-    .exec(http("Google").get("/"))
+  val scn = scenario("Google Load test")
+    .exec(http("Google").get("/"))      //Replace / with the path that you want to test
 
   setUp(
     scn.inject(
